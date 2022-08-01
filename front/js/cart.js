9ab377a13fd99ce5d5237827ce21fa7a8ecf935d
godhramn <jean.degrange@gmail.com>
2022-08-01 23:59:50 +0200

    added input text changer

--- front/js/cart.js
+++ front/js/cart.js
@@ -76,67 +76,69 @@ for (let i = 0; i < cart.length; i++) {
           settingsQuantity.appendChild(setQuantity);
           contentSettings.appendChild(settingsDelete);
           settingsDelete.appendChild(itemDelete);
-        }
+        
 
-        /* Modification de la quantité du produit */
+          /* Modification de la quantité du produit */
 
-        function changeQuantity(){
-          setQuantity.addEventListener("change", updateQuantity);
-          function updateQuantity() {
-            if (parseInt(setQuantity.value, 10) > 100) {
-              alert("Vous ne pouvez pas commander plus de 100 produits de même type");
-              setQuantity.value = "100";
-            } else if (parseInt(setQuantity.value, 10) < 1) {
-              alert("Quantité minimum de 1 produit, ou bien veuillez supprimer le produit du panier")
-              setQuantity.value = "1";
-            } else {
+          function changeQuantity(){
+            setQuantity.addEventListener("change", updateQuantity);
+            function updateQuantity() {
+              if (parseInt(setQuantity.value, 10) > 100) {
+                alert("Vous ne pouvez pas commander plus de 100 produits de même type");
+                setQuantity.value = "100";
+              } else if (parseInt(setQuantity.value, 10) < 1) {
+                alert("Quantité minimum de 1 produit, ou bien veuillez supprimer le produit du panier")
+                setQuantity.value = "1";
+              } else {
+                let cart = JSON.parse(localStorage.getItem("cart"));
+                cart[i].quantity = Math.floor(setQuantity.value);
+                localStorage.setItem("cart", JSON.stringify(cart));
+                window.location.reload();
+              }
+            }
+          }
+
+          /* Suppression du produit */
+
+          function deleteProduct(){
+            itemDelete.addEventListener("click", updateProducts);
+            function updateProducts(){
               let cart = JSON.parse(localStorage.getItem("cart"));
-              cart[i].quantity = Math.floor(setQuantity.value);
+              cart.splice(i, 1);
               localStorage.setItem("cart", JSON.stringify(cart));
               window.location.reload();
             }
           }
-        }
 
-        /* Suppression du produit */
+          /*créer mes éléments html de la section cart__quantity*/
 
-        function deleteProduct(){
-          itemDelete.addEventListener("click", updateProducts);
-          function updateProducts(){
-            let cart = JSON.parse(localStorage.getItem("cart"));
-            cart.splice(i, 1);
-            localStorage.setItem("cart", JSON.stringify(cart));
-            window.location.reload();
+          /* == afficher le total d'articles */
+          function displayQuantity() {
+            arrayQuantity.push(parseInt(cart[i].quantity));
+            const sum = arrayQuantity.reduce((previousValue, currentValue)  => previousValue + currentValue, 0);
+            const totalQuantity = document.querySelector("#totalQuantity");
+            totalQuantity.textContent = sum;
           }
+
+          /*créer mes éléments html de la section cart__price*/
+          /* == afficher le prix total */
+          function displayPrice() {
+            prices.push(cart[i].quantity * products.price)
+            const price = prices.reduce((previousValue, currentValue) => previousValue + currentValue, 0);
+            const totalPrice = document.querySelector("#totalPrice");
+            totalPrice.textContent = price;
+          }
+
+          /* Exécuter les fonctions */
+
+          changeQuantity(); /* Changer la quantité du produit */
+          deleteProduct(); /* Supprimer le produit */
+          displayQuantity(); /* Afficher la quantité totale d'articles */
+          displayPrice(); /* Afficher le prix total */
+          productsId.push(cart[i].id);
         }
-
-        /*créer mes éléments html de la section cart__quantity*/
-
-        /* == afficher le total d'articles */
-        function displayQuantity() {
-          arrayQuantity.push(parseInt(cart[i].quantity));
-          const sum = arrayQuantity.reduce((previousValue, currentValue)  => previousValue + currentValue, 0);
-          const totalQuantity = document.querySelector("#totalQuantity");
-          totalQuantity.textContent = sum;
-        }
-
-        /*créer mes éléments html de la section cart__price*/
-        /* == afficher le prix total */
-        function displayPrice() {
-          prices.push(cart[i].quantity * products.price)
-          const price = prices.reduce((previousValue, currentValue) => previousValue + currentValue, 0);
-          const totalPrice = document.querySelector("#totalPrice");
-          totalPrice.textContent = price;
-        }
-
-        /* Exécuter les fonctions */
-        displayProducts(); /* Afficher les produits */
-        changeQuantity(); /* Changer la quantité du produit */
-        deleteProduct(); /* Supprimer le produit */
-        displayQuantity(); /* Afficher la quantité totale d'articles */
-        displayPrice(); /* Afficher le prix total */
-        productsId.push(cart[i].id);
       }
+      displayProducts(); /* Afficher les produits */
     } catch (e) {
       console.log(e);
     }
@@ -145,49 +147,69 @@ for (let i = 0; i < cart.length; i++) {
 
 /* PASSER LA COMMANDE */
 
-/* vérifier le formulaire */
-
 const firstName = document.querySelector("#firstName");
 const lastName = document.querySelector("#lastName");
 const address = document.querySelector("#address");
 const city = document.querySelector("#city");
 const email = document.querySelector("#email");
 
-let textRegExp = new RegExp("^[A-Za-zÀ-ÿ'-]+[^0-9_!¿/+=@#$%&(){}|~<>;:]$");
+let clientForm = [
+  firstName,
+  lastName,
+  address,
+  city,
+  email
+]
+
+let textRegExp = new RegExp("^[A-Za-zÀ-ÿ '-]+[^0-9_!¿/+=@#$%&(){}|~<>;:]$");
 let addressRegExp = new RegExp("^[0-9a-zA-ZÀ-ÿ\s,-. ]+$");
 let emailRegExp = new RegExp("[A-Za-z0-9]+@[A-Za-z.-]+.[A-Za-z]{2,3}$");
 
-/* Tester la saisie du formulaire */
+let errorMsg = [
+  document.querySelector("#firstNameErrorMsg"),
+  document.querySelector("#lastNameErrorMsg"),
+  document.querySelector("#addressErrorMsg"),
+  document.querySelector("#cityErrorMsg"),
+  document.querySelector("#emailErrorMsg")
+]
+
+
+/* Mettre en forme la saisie */
+
+function normalizeTextInput() {
+  for( let i = 0; i < clientForm.length; i++ ) {
+    clientForm[i].addEventListener("change", changeTextForm);
+    function changeTextForm() {
+      if (clientForm[i].value.length > 1) {
+        if (i == 0) {
+          clientForm[i].value = clientForm[i].value[0].toUpperCase() + clientForm[i].value.substring(1);
+        } else if 
+          (i > 0 && i < 4) {
+          clientForm[i].value = clientForm[i].value.toUpperCase()
+        } else if (i == 4){
+          clientForm[i].value = clientForm[i].value.toLowerCase()
+        }
+      }
+    }
+  }
+}
+
+/* Tester les saisies du formulaire */
 
 function checkInput() {
 
   let tests = [
-  textRegExp.test(firstName.value),
-  textRegExp.test(lastName.value),
-  addressRegExp.test(address.value),
-  textRegExp.test(city.value),
-  emailRegExp.test(email.value)
+    textRegExp.test(firstName.value),
+    textRegExp.test(lastName.value),
+    addressRegExp.test(address.value),
+    textRegExp.test(city.value),
+    emailRegExp.test(email.value)
   ]
 
-  let clientForm = [
-    firstName,
-    lastName,
-    address,
-    city,
-    email
-  ]
-
-  let errorMsg = [
-    document.querySelector("#firstNameErrorMsg"),
-    document.querySelector("#lastNameErrorMsg"),
-    document.querySelector("#addressErrorMsg"),
-    document.querySelector("#cityErrorMsg"),
-    document.querySelector("#emailErrorMsg")
-  ]
-
-  /* Signaler les saisies valides ou erronées */
+  /* Signaler les champs valides ou erronées */
 
   for (let i = 0; i < tests.length; i++) {
+    
     let validInput = "solid green";
     let invalidInput = "solid red";
 
@@ -209,31 +231,29 @@ function validateForm () {
       addressRegExp.test(address.value) == true &&
       textRegExp.test(city.value) == true &&
       emailRegExp.test(email.value) == true) {
-        return true
-      } else {
-        return false
-      }
+    return true
+  } else {
+    return false
+  }
 }
 
 /* Commander */
 
 function placeOrder() {
   const orderClick = document.querySelector("#order");
-
-  orderClick.addEventListener("click", sendOrder, false);
+  orderClick.addEventListener("click", sendOrder);
 
   function sendOrder(submit) {
-    
     submit.preventDefault();
 
-    /* Valider la commande */
-    
     if (cart.length == 0){
       alert("Vous n'avez aucun article dans le panier")
     } else {
 
-      checkInput() /* Vérifier la saisie du formulaire */
+      checkInput() /* Vérifier la saisie des champs */
       validateForm () /* Vérifier si le formulaire est correctement rempli */
+      
+      /* Valider la commande */
 
       if (validateForm() == true) {
 
@@ -270,4 +290,5 @@ function placeOrder() {
   }
 }
 
-placeOrder()
+normalizeTextInput() /* Mettre en forme la saisie */
+placeOrder() /* Vérifier la commande */
